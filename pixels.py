08976eccf48497a20f208ae01ea8dbd245f30899
Karan Goel <karan@goel.im>
2013-08-04 14:00:17 +0530

    Gets RGB value of every pixel on screen

--- pixels.py
+++ pixels.py
@@ -3,9 +3,17 @@ Gets pixels every X minutes and draws an image out of them.
 """
 
 import ImageGrab
+from collections import defaultdict
 
 if __name__ == '__main__':
 
-    while True:
-        img = ImageGrab.grab() # take a screenshot
-        
+    img = ImageGrab.grab() # take a screenshot
+    (width, height) = img.size # get the image (screen) size
+
+    data = defaultdict(tuple) # {(x, y): (r, g, b), ...}
+    
+    for w in xrange(width):
+        for h in xrange(height):
+            data[(w, h)] = img.getpixel((w, h))
+
+    print data
