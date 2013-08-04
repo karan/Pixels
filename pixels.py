a246d4bc885cb99d8aed4e166bae4b542cc7c18a
Karan Goel <karan@goel.im>
2013-08-04 14:20:31 +0530

    Get a pixel every X seconds

--- pixels.py
+++ pixels.py
@@ -1,21 +1,23 @@
 """
-Gets pixels every X minutes and draws an image out of them.
+Gets pixels every X seconds and draws an image out of them.
 """
 
 import ImageGrab
 from collections import defaultdict
 from time import sleep
 
-if __name__ == '__main__':
 
-    img = ImageGrab.grab() # take a screenshot
-    img = img.resize((100, 100)) # create a thumbnail
-    (width, height) = img.size # get the image (screen) size
+X = 5 # seconds to pause before taking next pixel
+
+if __name__ == '__main__':
 
     data = defaultdict(tuple) # {(x, y): (r, g, b), ...}
     
     for w in xrange(100):
         for h in xrange(100):
+            img = ImageGrab.grab() # take a screenshot
+            img = img.resize((100, 100)) # create a thumbnail
             data[(w, h)] = img.getpixel((w, h))
+            sleep(X)
 
     print data
