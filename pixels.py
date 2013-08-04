2e81089b2486696158aad30b0d5cead2e533a325
Karan Goel <karan@goel.im>
2013-08-04 15:12:38 +0530

    Write pixel info to png

--- pixels.py
+++ pixels.py
@@ -2,22 +2,32 @@
 Gets pixels every X seconds and draws an image out of them.
 """
 
-import ImageGrab
+import time
 from collections import defaultdict
-from time import sleep
+
+import Image
+import ImageGrab
+import ImageDraw
 
 
-X = 5 # seconds to pause before taking next pixel
+X = 0 # seconds to pause before taking next pixel
 
 if __name__ == '__main__':
 
-    data = defaultdict(tuple) # {(x, y): (r, g, b), ...}
+    start = time.time()
     
-    for w in xrange(100):
-        for h in xrange(100):
-            img = ImageGrab.grab() # take a screenshot
-            img = img.resize((100, 100)) # create a thumbnail
-            data[(w, h)] = img.getpixel((w, h))
-            sleep(X)
+    data = defaultdict(tuple) # {(x, y): (r, g, b), ...}
 
-    print data
+    pixels_img = Image.new('RGB', (200, 200), 'black') # create a new Image instance
+    pixels = pixels_img.load()
+    
+    for w in xrange(1, 200):
+        for h in xrange(1, 200):
+            img = ImageGrab.grab() # take a screenshot
+            img = img.resize((200, 200)) # create a thumbnail
+            #data[(w, h)] = img.getpixel((w, h))
+            pixels[w, h] = img.getpixel((w, h))
+
+    pixels_img.save('pixel.png', 'PNG')
+    print 'done'
+    print time.time() - start
