61fc2a84bb96e935db41d228f966e22683836915
Karan Goel <karan@goel.im>
2013-08-04 14:18:45 +0530

    Work with thumbnail to increase performance

--- pixels.py
+++ pixels.py
@@ -4,16 +4,18 @@ Gets pixels every X minutes and draws an image out of them.
 
 import ImageGrab
 from collections import defaultdict
+from time import sleep
 
 if __name__ == '__main__':
 
     img = ImageGrab.grab() # take a screenshot
+    img = img.resize((100, 100)) # create a thumbnail
     (width, height) = img.size # get the image (screen) size
 
     data = defaultdict(tuple) # {(x, y): (r, g, b), ...}
     
-    for w in xrange(width):
-        for h in xrange(height):
+    for w in xrange(100):
+        for h in xrange(100):
             data[(w, h)] = img.getpixel((w, h))
 
     print data
