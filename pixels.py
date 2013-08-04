e83e5519608d6a7d2cbbbf1777ecc31011a8da02
Karan Goel <karan@goel.im>
2013-08-04 16:11:19 +0530

    Final thing

--- pixels.py
+++ pixels.py
@@ -1,33 +1,31 @@
 """
 Gets pixels every X seconds and draws an image out of them.
+A lazy Sunday afternoon hack!
 """
 
 import time
-from collections import defaultdict
 
 import Image
 import ImageGrab
-import ImageDraw
 
 
-X = 0 # seconds to pause before taking next pixel
+X = 1 # seconds to pause before taking next pixel
+size = 300 # the width=height of the image drawn, higher number = slower
 
 if __name__ == '__main__':
 
-    start = time.time()
+    #start = time.time()
     
-    data = defaultdict(tuple) # {(x, y): (r, g, b), ...}
-
-    pixels_img = Image.new('RGB', (200, 200), 'black') # create a new Image instance
+    pixels_img = Image.new('RGB', (size, size), 'black') # create a new Image instance
     pixels = pixels_img.load()
     
-    for w in xrange(1, 200):
-        for h in xrange(1, 200):
+    for w in xrange(1, size):
+        for h in xrange(1, size):
             img = ImageGrab.grab() # take a screenshot
-            img = img.resize((200, 200)) # create a thumbnail
-            #data[(w, h)] = img.getpixel((w, h))
-            pixels[w, h] = img.getpixel((w, h))
+            img = img.resize((size, size)) # create a thumbnail
+            pixels[w, h] = img.getpixel((w, h)) # pixels(x, y) = (r, g, b)
+            time.sleep(X)
 
     pixels_img.save('pixel.png', 'PNG')
-    print 'done'
-    print time.time() - start
+    #print 'done'
+    #print time.time() - start
