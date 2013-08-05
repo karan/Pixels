3ce2763400c6fb7106073bae84e2f0d2a83d2190
Karan Goel <karan@goel.im>
2013-08-05 13:08:48 +0530

    Much faster, add sample

--- pixels.py
+++ pixels.py
@@ -9,13 +9,13 @@ import Image
 import ImageGrab
 
 
-X = 2 # seconds to pause before taking next pixel
-WIDTH = 1920 # the width of the image drawn, higher number = slower
-HEIGHT = 1080 # the height of the image drawn, higher number = slower
+X = 1 # seconds to pause before taking next pixel
+WIDTH = 600 # the width of the image drawn, higher number = slower
+HEIGHT = 600 # the height of the image drawn, higher number = slower
 
 if __name__ == '__main__':
 
-    #start = time.time()
+    start = time.time()
     
     pixels_img = Image.new('RGB', (WIDTH, HEIGHT), 'black') # create a new Image instance
     pixels = pixels_img.load()
@@ -25,8 +25,9 @@ if __name__ == '__main__':
         img = img.resize((WIDTH, HEIGHT)) # create a thumbnail
         for h in xrange(HEIGHT):
             pixels[w, h] = img.getpixel((w, h)) # pixels(x, y) = (r, g, b)
-            time.sleep(X)
+        time.sleep(X)
 
     pixels_img.save('pixel.png', 'PNG')
     pixels_img.show()
-    #print time.time() - start
+
+    print time.time() - start
